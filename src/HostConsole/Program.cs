3bd22ae74eb66217d3e96f3b955efe39c7ab18df
nahueltaibo <nahueltaibo@gmail.com>
2019-11-06 12:59:10 -0500

    Starting from clean net core console app template

---
+++ src/HostConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HostConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
