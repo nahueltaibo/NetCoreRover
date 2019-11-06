322d8e0e5c11880b691db99bbb5fbe446fd3e15a
nahueltaibo <nahueltaibo@gmail.com>
2019-11-06 13:40:38 -0500

    Project HostConsole renamed to Rover.Host Added code to wait for debugger if the app is started with a "debug" as first parameter

---
+++ src/Rover.Host/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace HostConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // If the app is called with "debug" as first argument, it will wait in the following loop for
+            // you to attach to the process to debug. Add a breakpoint within the while, and once you
+            // attach you connect the debugger to the process, manually move the execution outside the while body
+            // so the application can run with the debugger attached from the beginning
+            if (args.Length > 0 && args[0].ToLower().Contains("debug"))
+            {
+                Console.WriteLine("Waiting for debugger to attach...");
+                while (!Debugger.IsAttached)
+                {
+                    Thread.Sleep(100);
+                }
+                Console.WriteLine("Debugger attached.");
+            }
+        }
+    }
+}
