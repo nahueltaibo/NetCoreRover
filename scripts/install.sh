f70fd6c94c69360b9cdde471f7dc67b161dd7613
nahueltaibo <nahueltaibo@gmail.com>
2020-02-24 19:28:46 -0500

    Added dotnet install verification at the end

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+DOTNET_VERSION=2.2.207
 DOTNET_INSTALLER=dotnet-sdk-2.2.207-linux-arm.tar.gz
 DOTNET_INSTALLER_URL=https://download.visualstudio.microsoft.com/download/pr/fca1c415-b70c-4134-8844-ea947f410aad/901a86c12be90a67ec37cd0cc59d5070/dotnet-sdk-2.2.207-linux-arm.tar.gz
 DOTNET_LOCATION=$HOME/dotnet
@@ -15,21 +16,20 @@ clear
 echo "${green}**** NetCoreRover Environment Installer ****${reset}"
 
 echo
-#echo "${green}Updating Raspberry Pi...${reset}"
-#sudo apt-get update
+echo "${green}Updating Raspberry Pi...${reset}"
+sudo apt-get update
 
-#echo
-#echo "${green}Upgrading Raspberry Pi...${reset}"
-#sudo apt-get upgrade -y
+echo
+echo "${green}Upgrading Raspberry Pi...${reset}"
+sudo apt-get upgrade -y
 
-#echo
-#echo "${green}Upgrading Raspberry Pi distro...${reset}"
-#sudo apt-get dist-upgrade -y
+echo
+echo "${green}Upgrading Raspberry Pi distro...${reset}"
+sudo apt-get dist-upgrade -y
 
 echo
 echo "${green}Installing required dependencies...${reset}"
 sudo apt-get install curl libunwind8 gettext
-
 
 # Download dotnet only if needed...
 if [ -f "$DOTNET_INSTALLER" ]; then
@@ -43,15 +43,14 @@ fi
 
 # if dotnet is already installed, remove it first
 if [ -d "$DOTNET_LOCATION" ]; then
-    echo
-    echo "${green}netcore already installed, removing it first.${reset}"
-    rm -r "$DOTNET_LOCATION"
-fi
-mkdir -p "$DOTNET_LOCATION"
+    echo "${yellow}netcore already installed, avoiding installation.${reset}"
+else
+    mkdir -p "$DOTNET_LOCATION"
 
-#echo
-#echo "${green}Extracting netcore to $DOTNET_LOCATION...${reset}"
-#tar zxf "$DOTNET_INSTALLER" -C "$DOTNET_LOCATION"
+    echo
+    echo "${green}Extracting netcore to $DOTNET_LOCATION...${reset}"
+    tar zxf "$DOTNET_INSTALLER" -C "$DOTNET_LOCATION"
+fi
 
 #echo
 #echo "${green}removing dotnet installer...${reset}"
@@ -70,6 +69,17 @@ else
     echo Adding PATH configuration...
     echo -e "\nPATH=\$PATH:$DOTNET_LOCATION" >> ~/.bashrc
 fi  
+
+# Check if dotnet was correctly installed
+echo 
+echo dotnet version:
+if dotnet --version | grep $DOTNET_VERSION; then
+    echo
+    echo "${green}dotnet is correctly installed${reset}"
+else
+    echo
+    echo "${red}there was an error installing dotnet version $DOTNET_VERSION${reset}"
+fi
 
 echo
 echo "${yellow}Remember to enable SSH to publish!${reset}"
