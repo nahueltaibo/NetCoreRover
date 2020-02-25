bcc2b3ca7606a87b45c192a23d1f0f802f7588bb
nahueltaibo <nahueltaibo@gmail.com>
2020-02-24 23:21:36 -0500

    MessageBus first steps #WIP

---
+++ src/MessageBus/IMessageBroker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MessageBus.Messages
+{
+    public interface IMessageBroker
+    {
+        Task PublishAsync<T>(string topic, T message);
+
+        Task SubscribeAsync<T>(string topic, Action<IMessage> callback);
+    }
+}
---
+++ src/MessageBus/Subscription.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MessageBus
+{
+    internal class Subscription
+    {
+        public Type Type { get; internal set; }
+        public string Topic { get; internal set; }
+        public Action<IMessage> Callback { get; internal set; }
+    }
+}
